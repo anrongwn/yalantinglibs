95ccd9e7619577b04b96a9e77b893b7b91225a57
PikachuHy <pikachuhy@linux.alibaba.com>
2022-11-11 15:24:04 +0800

    use pb verify buf

--- src/struct_pack/tests/test_pb.cpp
+++ src/struct_pack/tests/test_pb.cpp
@@ -15,6 +15,7 @@
  */
 #include "doctest.h"
 #include "struct_pack/struct_pack/pb.hpp"
+#include "test_pb.pb.h"
 using namespace doctest;
 using namespace struct_pack::pb;
 struct test1 {
@@ -52,10 +53,15 @@ TEST_CASE("testing test1") {
     CHECK(t.a == d_t.a);
   }
   SUBCASE("has value") {
+    std::string buf{0x08, (char)0x96, 0x01};
+    Test1 pb_t;
+    pb_t.set_a(150);
+    auto pb_buf = pb_t.SerializeAsString();
+    REQUIRE(pb_buf == buf);
+
     t.a = 150;
     auto size = get_needed_size(t);
     REQUIRE(size == 3);
-    std::string buf{0x08, (char)0x96, 0x01};
     auto b = serialize<std::string>(t);
     CHECK(buf == b);
     std::size_t len = 0;
@@ -81,11 +87,18 @@ TEST_CASE("testing test2") {
     auto d_t = d_t_ret.value();
   }
   SUBCASE("has value") {
-    t.b = "testing";
+    std::string buf{0x12, 0x07};
+    std::string s = "testing";
+    buf += s;
+
+    Test2 pb_t;
+    pb_t.set_b(s);
+    auto pb_buf = pb_t.SerializeAsString();
+    REQUIRE(pb_buf == buf);
+
+    t.b = s;
     auto size = get_needed_size(t);
     REQUIRE(size == 9);
-    std::string buf{0x12, 0x07};
-    buf += t.b.value();
     auto b = serialize<std::string>(t);
     CHECK(buf == b);
     std::size_t len = 0;
@@ -111,10 +124,18 @@ TEST_CASE("testing test3") {
     auto d_t = d_t_ret.value();
   }
   SUBCASE("has value") {
+    std::string buf{0x1a, 0x03, 0x08, (char)0x96, 0x01};
+
+    Test3 pb_t;
+    auto pb_c = new Test1;
+    pb_c->set_a(150);
+    pb_t.set_allocated_c(pb_c);
+    auto pb_buf = pb_t.SerializeAsString();
+    REQUIRE(pb_buf == buf);
+
     t.c = test1{.a = 150};
     auto size = get_needed_size(t);
     REQUIRE(size == 5);
-    std::string buf{0x1a, 0x03, 0x08, (char)0x96, 0x01};
     auto b = serialize<std::string>(t);
     CHECK(buf == b);
     std::size_t len = 0;
@@ -140,6 +161,11 @@ TEST_CASE("testing test4") {
     auto d_t = d_t_ret.value();
   }
   SUBCASE("string empty") {
+    Test4 pb_t;
+    pb_t.add_e(1);
+    auto pb_buf = pb_t.SerializeAsString();
+    REQUIRE(pb_buf.size() == 3);
+
     t.e = {1};
     auto size = get_needed_size(t);
     REQUIRE(size == 3);
@@ -157,11 +183,18 @@ TEST_CASE("testing test4") {
     CHECK(t.e[0] == d_t.e[0]);
   }
   SUBCASE("repeated empty") {
-    t.d = "hi";
-    auto size = get_needed_size(t);
-    REQUIRE(size == 4);
     // [4] 22 2 68 69
     std::string buf{0x22, 0x02, 0x68, 0x69};
+    std::string s = "hi";
+
+    Test4 pb_t;
+    pb_t.set_d(s);
+    auto pb_buf = pb_t.SerializeAsString();
+    REQUIRE(pb_buf == buf);
+
+    t.d = s;
+    auto size = get_needed_size(t);
+    REQUIRE(size == 4);
     auto b = serialize<std::string>(t);
     CHECK(buf == b);
     std::size_t len = 0;
@@ -174,6 +207,14 @@ TEST_CASE("testing test4") {
     CHECK(t.d == d_t.d);
   }
   SUBCASE("has value") {
+    Test4 pb_t;
+    pb_t.set_d("hello");
+    pb_t.add_e(1);
+    pb_t.add_e(2);
+    pb_t.add_e(3);
+    auto pb_buf = pb_t.SerializeAsString();
+    REQUIRE(pb_buf.size() == 12);
+
     t.d = "hello";
     t.e = {1, 2, 3};
     auto size = get_needed_size(t);
@@ -186,7 +227,7 @@ TEST_CASE("testing test4") {
     // 34 5 'h' 'e' 'l' 'l' 'o' 42 3 1 2 3
     // 22 5 68 65 6c 6c 6f 2a 3 1 2 3
     auto b = serialize<std::string>(t);
-    CHECK(buf == b);
+    REQUIRE(buf == b);
     std::size_t len = 0;
     auto d_t_ret = deserialize<test4>(b.data(), b.size(), len);
     CHECK(len == 12);
@@ -204,14 +245,23 @@ struct my_test_float {
   float e;
 };
 TEST_CASE("testing float") {
-  my_test_float t{.a = 0, .b = 1, .c = -1, .d = 1.234, .e = 1.234e5};
-  auto size = get_needed_size(t);
-  REQUIRE(size == 20);
   // [20] 15 00 00 80 3f 1d 00 00 80 bf 25 b6 f3 9d 3f 2d 00 04 f1 47
   std::string buf{0x15, 0x00,       0x00,       (char)0x80, 0x3f,
                   0x1d, 0x00,       0x00,       (char)0x80, (char)0xbf,
                   0x25, (char)0xb6, (char)0xf3, (char)0x9d, 0x3f,
                   0x2d, 0x00,       0x04,       (char)0xf1, 0x47};
+  MyTestFloat pb_t;
+  pb_t.set_a(0);
+  pb_t.set_b(1);
+  pb_t.set_c(-1);
+  pb_t.set_d(1.234);
+  pb_t.set_e(1.234e5);
+  auto pb_buf = pb_t.SerializeAsString();
+  REQUIRE(pb_buf == buf);
+
+  my_test_float t{.a = 0, .b = 1, .c = -1, .d = 1.234, .e = 1.234e5};
+  auto size = get_needed_size(t);
+  REQUIRE(size == 20);
   auto b = serialize<std::string>(t);
   CHECK(buf == b);
   std::size_t len = 0;
