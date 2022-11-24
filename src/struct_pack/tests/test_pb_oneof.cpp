57a337f416a0c1411e706955831b235d7a87479c
PikachuHy <pikachuhy@linux.alibaba.com>
2022-11-24 09:43:14 +0800

    add oneof test

---
+++ src/struct_pack/tests/test_pb_oneof.cpp
@@ -0,0 +1,102 @@
+#include "struct_pack/struct_pack/pb.hpp"
+#include "doctest.h"
+#include "test_pb.pb.h"
+#include "hex_printer.hpp"
+using namespace struct_pack::pb;
+using namespace doctest;
+struct sub_message_for_oneof {
+  bool ok;
+  bool operator==(const sub_message_for_oneof&) const = default;
+};
+struct sample_message_oneof {
+  std::variant<varint32_t, varint32_t, std::string, sub_message_for_oneof> t;
+};
+template<>
+constexpr oneof_field_number_array_t<sample_message_oneof, 0>
+    oneof_field_number_seq<sample_message_oneof, 0>{
+    10, 8, 4, 9
+};
+
+
+TEST_CASE("testing oneof") {
+  SUBCASE("index 0") {
+    SampleMessageOneof pb_t;
+    pb_t.set_b(13298);
+    auto pb_buf = pb_t.SerializeAsString();
+
+    sample_message_oneof t;
+    t.t.emplace<0>(13298);
+    auto size = get_needed_size(t);
+    REQUIRE(size == pb_buf.size());
+    auto buf = serialize<std::string>(t);
+    REQUIRE(buf == pb_buf);
+    std::size_t len = 0;
+    auto d_t_ret = deserialize<sample_message_oneof>(buf.data(), buf.size(), len);
+    REQUIRE(d_t_ret);
+    REQUIRE(len == buf.size());
+    auto d_t = d_t_ret.value();
+    CHECK(d_t.t.index() == 0);
+    CHECK(std::get<0>(d_t.t) == 13298);
+  }
+  SUBCASE("index 1") {
+
+    SampleMessageOneof pb_t;
+    pb_t.set_a(66613298);
+    auto pb_buf = pb_t.SerializeAsString();
+
+    sample_message_oneof t;
+    t.t.emplace<1>(66613298);
+    auto size = get_needed_size(t);
+    REQUIRE(size == pb_buf.size());
+    auto buf = serialize<std::string>(t);
+    REQUIRE(buf == pb_buf);
+    std::size_t len = 0;
+    auto d_t_ret = deserialize<sample_message_oneof>(buf.data(), buf.size(), len);
+    REQUIRE(d_t_ret);
+    REQUIRE(len == buf.size());
+    auto d_t = d_t_ret.value();
+    CHECK(d_t.t.index() == 1);
+    CHECK(std::get<1>(d_t.t) == 66613298);
+  }
+  SUBCASE("index 2") {
+    SampleMessageOneof pb_t;
+    pb_t.set_name("oneof index 2");
+    auto pb_buf = pb_t.SerializeAsString();
+
+    sample_message_oneof t;
+    t.t.emplace<2>("oneof index 2");
+    auto size = get_needed_size(t);
+    REQUIRE(size == pb_buf.size());
+    auto buf = serialize<std::string>(t);
+    REQUIRE(buf == pb_buf);
+    std::size_t len = 0;
+    auto d_t_ret = deserialize<sample_message_oneof>(buf.data(), buf.size(), len);
+    REQUIRE(d_t_ret);
+    REQUIRE(len == buf.size());
+    auto d_t = d_t_ret.value();
+    CHECK(d_t.t.index() == 2);
+    CHECK(std::get<2>(d_t.t) == "oneof index 2");
+  }
+  SUBCASE("submessage index 3") {
+    SampleMessageOneof pb_t;
+    auto m = new SubMessageForOneof();
+    m->set_ok(true);
+    pb_t.set_allocated_sub_message(m);
+    auto pb_buf = pb_t.SerializeAsString();
+
+    sample_message_oneof t;
+    auto sub = sub_message_for_oneof{true};
+    t.t.emplace<3>(sub);
+    auto size = get_needed_size(t);
+    REQUIRE(size == pb_buf.size());
+    auto buf = serialize<std::string>(t);
+    REQUIRE(buf == pb_buf);
+    std::size_t len = 0;
+    auto d_t_ret = deserialize<sample_message_oneof>(buf.data(), buf.size(), len);
+    REQUIRE(d_t_ret);
+    REQUIRE(len == buf.size());
+    auto d_t = d_t_ret.value();
+    CHECK(d_t.t.index() == 3);
+    CHECK(std::get<3>(d_t.t) == sub);
+  }
+}
